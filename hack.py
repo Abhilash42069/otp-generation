04749ed958ecfd7d7c6a885b9a0fcbe479cd5d47
Ab69420 <59367876+Ab69420@users.noreply.github.com>
2019-12-30 21:33:20 +0530

    Add files via upload

---
+++ hack.py
@@ -0,0 +1,20 @@
+import smtplib
+import random as r
+import pyttsx3
+eng = pyttsx3.init()
+
+def otp_gen():
+    return ''.join([str(r.randint(1,9)) for i in range(4)])
+otp = otp_gen()
+eng.say('your otp is '+otp)
+eng.runAndWait()
+
+conn = smtplib.SMTP('smtp.gmail.com', 587)
+conn.ehlo()
+conn.starttls()
+conn.login('abhilash.biswas2000@gmail.com', 'yanyanbadbadthullu')
+conn.sendmail('wadethecuboidalfox@gmail.com', 'abone838@gmail.com',\
+              'Subject:Alert\n\n your otp:'+ otp_gen())
+conn.quit()
+eng.say('Your OTP is mailed')
+eng.runAndWait()
