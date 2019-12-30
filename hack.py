c52c45f35fbca446ad684fc5940481a2cabf3e01
Ab69420 <59367876+Ab69420@users.noreply.github.com>
2019-12-30 21:38:42 +0530

    Update hack.py

--- hack.py
+++ hack.py
@@ -12,9 +12,9 @@ eng.runAndWait()
 conn = smtplib.SMTP('smtp.gmail.com', 587)
 conn.ehlo()
 conn.starttls()
-conn.login('sender's email', 'password')  //enter the email and password of the sender
-conn.sendmail('recipient's email',\
-              'Subject:Alert\n\n your otp:'+ otp_gen()) //enter email of the reciever 
+conn.login('abc@gmail.com', 'password')
+conn.sendmail('xyz@gmail.com', 'pqr@gmail.com',\
+              'Subject:Alert\n\n your otp:'+ otp_gen())
 conn.quit()
 eng.say('Your OTP is mailed')
 eng.runAndWait()
